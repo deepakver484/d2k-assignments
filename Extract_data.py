904084f130241856b359a33b1187c64a5a116f02
deepakver484 <deepakkumarverma484@gmail.com>
2024-07-09 21:59:14 +0530

    added call_function which will check weather link is working or not and return the reponse

--- Extract_data.py
+++ Extract_data.py
@@ -2,23 +2,30 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import time
+import logging
 
 link = 'https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
 
-# lets check the link if its working 
-call = requests.get(link)
-if call.status_code == 200:
-    print('link is working')
-else:
+
+logging.basicConfig(level=logging.INFO)  # Set logging level to INFO globally
+
+def call_func(link):
     retries = 5
-    for i in range(retries):
-        time.sleep(5)
-        call = requests.get(link)
-        if call.status_code == 200:
-            break
+    for attempt in range(retries):
+        response = requests.get(link)
+        if response.status_code == 200:
+            logging.info('Link is working')
+            return response  # Return the response if successful
         else:
-            continue
-        
+            logging.warning(f'Retry attempt {attempt + 1}. Status code: {response.status_code}')
+            time.sleep(5)  # Wait for 5 seconds before retrying
+
+    logging.error(f'Failed to fetch data from {link} after {retries} attempts')
+    return None  # Return None if all retries fail
+
+
+
+
 
         
 
