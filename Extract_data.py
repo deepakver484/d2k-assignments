d43ca78ea9aa14c715d3a4e9527c4c5353e5fe19
deepakver484 <deepakkumarverma484@gmail.com>
2024-07-10 10:03:32 +0530

    added new function get_data to get the details of each data file and save this dataframe as data_link.csv in scraped_data folder , also add few line of code into thee main.py to create new directory for scraped_data if its not available

--- Extract_data.py
+++ Extract_data.py
@@ -4,10 +4,6 @@ import requests
 import time
 import logging
 
-link = 'https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
-
-
-logging.basicConfig(level=logging.INFO)  # Set logging level to INFO globally
 
 def call_func(link):
     retries = 5
@@ -22,6 +18,36 @@ def call_func(link):
 
     logging.error(f'Failed to fetch data from {link} after {retries} attempts')
     return None  # Return None if all retries fail
+
+def extract_trip_type(url):
+    if 'yellow' in url:
+        return 'yellow_trips'
+    elif 'green' in url:
+        return 'green_trips'
+    elif 'fhvhv_trip' in url:
+        return 'fhvhv_trip'
+    elif 'fhv' in url:
+        return 'fhv_trips'
+    else:
+        return None
+    
+def get_data(response):
+    soup = BeautifulSoup(response.text, features='html.parser')
+    links = soup.find(attrs={"id": "faq2019"}).find_all('a', href=True)
+    data_list = [link['href'] for link in links]
+    df = pd.DataFrame(data_list, columns =['links'])
+    date_part = df['links'].str.split('/').str[-1]  # Extract the last part after splitting by '/'
+    df['date'] = date_part.str.replace(r'[^0-9-]', '',regex=True)
+    logging.info('data scraped sucessfully')
+    df['trip_type'] = df['links'].apply(extract_trip_type)
+    grouped = df.groupby(['date', 'trip_type']).first().reset_index()
+    pivot_df = grouped.pivot(index='date', columns='trip_type', values='links').reset_index()
+    logging.info('pivot data is ready to use')
+    return pivot_df
+
+
+    
+
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import logging
+import Extract_data as ed
+import os
 
 logging.basicConfig(
     filename='app.log',  # Name of the log file
-    level=logging.DEBUG,  # Logging level for capturing messages
+    level=logging.INFO,  # Logging level for capturing messages
     format='%(asctime)s %(levelname)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+link = 'https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
+
+responce = ed.call_func(link)
+data_link = ed.get_data(responce)
+
+directory = 'scraped_data'
+os.makedirs(directory, exist_ok=True)
+data_link.to_csv('scraped_data/data_link.csv', index=False)
